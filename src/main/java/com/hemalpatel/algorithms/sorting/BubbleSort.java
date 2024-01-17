5db8e13353aa115d4704c4352847af8c7993f665
patelhemal2210 <patelhemal230@gmail.com>
2024-01-17 18:09:23 -0500

    Optimized implementation for BubbleSort

--- src/main/java/com/hemalpatel/algorithms/sorting/BubbleSort.java
+++ src/main/java/com/hemalpatel/algorithms/sorting/BubbleSort.java
@@ -5,13 +5,20 @@ package main.java.com.hemalpatel.algorithms.sorting;
  */
 public class BubbleSort {
 
+    // TODO : This function can be modified to support generic instead of int later
+    // This function is deliberately implemented with int array to set up a basic logic for bubble sort
     public static int[] sort(int[] data) {
-        for (int i = 0; i < data.length; i++) {
+
+        // Swapped flag will keep a track if the remaining elements are already sorted
+        boolean swapped = true;
+        for (int i = 0; i < data.length && swapped; i++) {
+            swapped = false;
             for (int j = 0; j < data.length - i - 1; j++) {
                 if (data[j] > data[j + 1]) {
                     int temp = data[j];
                     data[j] = data[j + 1];
                     data[j + 1] = temp;
+                    swapped = true;
                 }
             }
         }
